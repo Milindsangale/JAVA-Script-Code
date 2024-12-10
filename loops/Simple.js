556a53931cef418206e9e6f987a4b2306e7b4c35
Milind Sangale <smilind900@gmail.com>
2024-12-10 19:36:38 +0530

    initial commit

--- loops/Simple.js
+++ loops/Simple.js
@@ -12,5 +12,41 @@ for(let i=1;i<=n;i++){
 
 
 for(let i=1;i<=10;i++){
-    console.log("Milind Raosaheb Sangale");
+    console.log("Milind");
 }
+
+
+
+// fot-of loop
+let length =0;
+let str ="Milind";
+for(let i of str) {
+  //  console.log("i=", i);
+    length++;
+    console.log("String length",length);
+}
+
+//for in loop
+
+
+//print all even numbers from 0 to 100
+for(let num=0; num<=100;num++){
+   // console.log("num=",num);
+   if(num%2 ==0){
+    console.log("even number",num);
+   }
+}
+
+
+// game
+let gameNum = 100;
+let usernumner =prompt("Guess the number");
+while(usernumner != gameNum){
+   usernumner= prompt("you entered wrong number. Guess again :");
+}
+    console.log("congratulations you entered the rigt number");
+
+
+
+
+let Str = "Milind sangale";
