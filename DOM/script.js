1fd330b4fe4e2fa07967aa79cab6cfe21fc75284
Milind Sangale <smilind900@gmail.com>
2024-12-11 20:24:20 +0530

    initial commit

--- DOM/script.js
+++ DOM/script.js
@@ -15,8 +15,50 @@
 //console.log(milind);
 //console.dir(milind);
 
-let milind = document.querySelectorAll(".heading-class");
+/* let milind = document.querySelectorAll(".heading-class");
 console.dir(milind);
-
+ */
 
 // DOM video53.48
+
+
+/* let milindsangale = document.getElementsByClassName("Milind-sangale")
+console.dir(milindsangale) */
+/* 
+let milindsangale = document,getElementsByTagName("p");
+console.log(milindsangale); */
+/* 
+let milindsangale =document.getElementsByClassName("milindsangale-class");
+console.log(milindsangale); */
+
+
+/* let Milind = document.getElementsByClassName("milindsangale-class");
+console.dir(Milind); */
+/* let div = document.querySelector("div");
+console.dir(div);
+ */
+
+
+//let h2 = document.querySelector("h2");
+/* console.dir(h2); */
+
+//console.dir(h2.innerText);
+//h2.innerText = h2.innerText +"from Milind Raosaheb sangale";  // Concannet 
+
+
+/* let divs = document.querySelectorAll(".milind");
+let idx = 1;
+for (div of divs){
+    div.innerText ='new unique value $(idz}';
+    idx++;
+} */
+/* divs[0].innerText = "new unique value 1 Milind sangale";
+divs[1].innerText = "new unique value 2 Milind Sangale";
+divs[2].innerText = "new unique value 3 Milind Sangale";
+  */
+
+ let div = document.querySelector("div");
+/*  div.style.backgroundColor ="black"; */
+
+let div = document.querySelector("Milind");
+console.log("Milind");
